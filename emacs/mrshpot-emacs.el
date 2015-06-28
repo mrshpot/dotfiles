04b9ff834de62a60062b685d47de23298dce3a93
Taras Shpot <mrshpot@gmail.com>
2015-06-28 23:17:16 +0300

    mrshpot-emacs: Add shell customizations

--- emacs/mrshpot-emacs.el
+++ emacs/mrshpot-emacs.el
@@ -114,6 +114,15 @@
 ;; shell
 ;; Fix shell-mode color special characters
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+;; shell customizations
+(custom-set-variables
+ '(comint-scroll-to-bottom-on-input t)  ; always insert at the bottom
+ ; '(comint-scroll-to-bottom-on-output t) ; always add output at the bottom
+ ; '(comint-scroll-show-maximum-output t) ; scroll to show max possible output
+ '(comint-completion-autolist t)        ; show completion list when ambiguous
+ '(comint-input-ignoredups t)           ; no duplicates in command history
+ '(comint-completion-addsuffix t)       ; insert space/slash after file completion
+ )
 
 ;; move between windows with M-arrows
 (windmove-default-keybindings 'meta)
