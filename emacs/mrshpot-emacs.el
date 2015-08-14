16f48b0847cc64237a5ac97d61db19de832f6877
Taras Shpot <taras.shpot@eleks.com>
2015-08-14 11:10:50 +0300

    Emacs: Add marmalade repo

--- emacs/mrshpot-emacs.el
+++ emacs/mrshpot-emacs.el
@@ -57,6 +57,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+			 '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
